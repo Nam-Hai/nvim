85d393f516886baafb878b8ed5f72b20579d31cf
Lucas TRAN <lucas.tran@b-reel.com>
2023-04-05 16:40:13 +0200

    null-ls out

--- after/plugin/null-ls.lua
+++ after/plugin/null-ls.lua
@@ -1,29 +1,29 @@
-local null_ls = require("null-ls")
-
-local formatting = null_ls.builtins.formatting
-local diagnostics = null_ls.builtins.diagnostics
-
-null_ls.setup({
-	sources = {
-		formatting.prettier,
-		formatting.stylua,
-		-- diagnostics.eslint_d,
-		-- null_ls.builtins.completion.spell,
-	},
-})
-
-local mason_null_ls_status, mason_null_ls = pcall(require, "mason-null-ls")
-if not mason_null_ls_status then
-	return
-end
-
-mason_null_ls.setup({
-	-- list of formatters & linters for mason to install
-	ensure_installed = {
-		"prettier", -- ts/js formatter
-		"stylua", -- lua formatter
-		-- "eslint_d", -- ts/js linter
-	},
-	-- auto-install configured formatters & linters (with null-ls)
-	automatic_installation = true,
-})
+-- local null_ls = require("null-ls")
+--
+-- local formatting = null_ls.builtins.formatting
+-- local diagnostics = null_ls.builtins.diagnostics
+--
+-- null_ls.setup({
+-- 	sources = {
+-- 		formatting.prettier,
+-- 		formatting.stylua,
+-- 		-- diagnostics.eslint_d,
+-- 		-- null_ls.builtins.completion.spell,
+-- 	},
+-- })
+--
+-- local mason_null_ls_status, mason_null_ls = pcall(require, "mason-null-ls")
+-- if not mason_null_ls_status then
+-- 	return
+-- end
+--
+-- mason_null_ls.setup({
+-- 	-- list of formatters & linters for mason to install
+-- 	ensure_installed = {
+-- 		"prettier", -- ts/js formatter
+-- 		"stylua", -- lua formatter
+-- 		-- "eslint_d", -- ts/js linter
+-- 	},
+-- 	-- auto-install configured formatters & linters (with null-ls)
+-- 	automatic_installation = true,
+-- })
